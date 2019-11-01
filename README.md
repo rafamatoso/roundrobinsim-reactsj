9a4f245c69be0ec715e061e1db1a14de3bb05cf5
rafamatoso <rafaelmatosog@gmail.com>
2019-11-01 12:13:40 -0300

    Projeto criado

---
+++ README.md
@@ -0,0 +1,8 @@
+# Análise e Monitoramento de Desempenho - 2019.2 - UniFBV | Wyden
+   
+Neste repositório existe duas pastas: 
+* **doc** - Documentações, Imagens e etc
+* **osqueue-app** - Aplicação React
+
+
+ 
---
+++ osqueue-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import imgGroup from './Img/fire785116.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={imgGroup} className="App-logo" alt="logo" />
+        <p className="text">
+          Nosso OSQueueApp começa aqui!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
